0aebdd4c22be73fcd8fc796c7328e64e6cff175b
unknown <ramsescastillo42@gmail.com>
2019-10-17 02:16:45 -0400

    placement changes take 2

--- mods/wordblocks/wordblocks.js
+++ mods/wordblocks/wordblocks.js
@@ -674,9 +674,9 @@ Wordblocks.prototype.addEventsToBoard = function addEventsToBoard() {
     let divname = $(this).attr("id");
     let html = `
       <div class="tile-placement-controls">
-        <span class="action fas fa-arrows-alt-h" id="horizontally">horizontally</span>
-        <span class="action fas fa-arrows-alt-v" id="vertically">vertically</span>
-        <span class="action far fa-window-close" id="cancel">cancel</span>
+      <i class="fas fa-arrows-alt-h"></i><span class="action" id="horizontally">horizontally</span>
+      <i class="fas fa-arrows-alt-v"></i><span class="action" id="vertically">vertically</span>
+      <i class="far fa-window-close"></i><span class="action" id="cancel">cancel</span>
       </div>`;
     let tmpx = divname.split("_");
     let y = tmpx[0];
