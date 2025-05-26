f963a077808e8877ea1cbf4ac5d5bbc677984c62
Umair Khan <umair@saito.tech>
2025-05-26 08:34:31 +0500

    Update

--- node/lib/saito/ui/saito-crypto/overlays/withdraw.template.js
+++ node/lib/saito/ui/saito-crypto/overlays/withdraw.template.js
@@ -17,9 +17,6 @@ module.exports = (app, mod, publickey = "", address = "") => {
               <div>
                  Send Token
               </div>
-              <div class="nft-link" id="nft-link">
-                 <span>send or create nft</span> 
-              </div>
            </div>
         </div>
 
