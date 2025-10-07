3621748fc9d9d670cc8a419fedf0dac9995b1f74
David Lancashire <david.lancashire@gmail.com>
2025-10-07 11:45:12 +0700

    fix: load transactios override

--- node/mods/assetstore/lib/overlays/assetstore-nft.js
+++ node/mods/assetstore/lib/overlays/assetstore-nft.js
@@ -8,6 +8,56 @@ class AssetStoreNft extends SaitoNFT {
 
   }
 
+
+
+  async fetchTransaction(callback = null) {
+    if (!this.id) {
+      console.error('Unable to fetch NFT transaction (no nft id found)');
+      if (callback) {
+        this.tx_fetched = false;
+        return callback();
+      }
+    }
+
+    if (this.tx && this.txmsg && (this.image || this.text)) {
+      //
+      // Avoiding fetchTransaction (tx, txmsg, img/txt already set);
+      //
+      if (callback) {
+        this.tx_fetched = false;
+        return callback();
+      }
+    }
+
+    await this.app.storage.loadTransactions(
+      { field4: this.id },
+
+      async (txs) => {
+        if (txs?.length > 0) {
+          this.tx = txs[0];
+          this.buildNFTData();
+          if (callback) {
+            this.tx_fetched = true;
+            return callback();
+          }
+        } else {
+
+if (this.app.BROWSER) {
+  alert("trying to fetch assetstore tx");
+}
+
+        }
+      },
+      'localhost'
+    );
+
+    this.tx_fetched = false;
+    return null;
+  }
+
+
+
+
 }
 
 module.exports = AssetStoreNft;
