42513e08e79cd0031780222e0339f3242f49aec0
david.lancashire@gmail.com <trevelyan@Davids-MacBook-Pro.local>
2026-01-15 11:01:21 +0700

    fix: vault patches

--- node/mods/vault/lib/ui/overlays/file-info.template.js
+++ node/mods/vault/lib/ui/overlays/file-info.template.js
@@ -17,13 +17,9 @@ module.exports = (app, mod, file_info = {}) => {
 
   <div class="vault-file-info-success" style="display:none; opacity:0;">
     <h2>Success!</h2>
-    <p>
       Your NFT access key is being finalized.
-    </p>
-    <p>
-      Once it arrives, you can access your file
-      <a href="/vault">from this page</a> anytime.
-    </p>
+      <p></p>
+      Once it arrives, <a href="/">access your file</a> here.
   </div>
 </div>
   `;
