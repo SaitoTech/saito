b74a0a057d9eeb593bc3a5a674e93ac45aa1b4ee
arpee <richard@saito.tech>
2026-01-28 13:37:44 +1100

    adding sol mod

--- node/mods/sol/sol.js
+++ node/mods/sol/sol.js
@@ -2,33 +2,40 @@ const saito = require('./../../lib/saito/saito');
 const ModTemplate = require('../../lib/templates/modtemplate');
 
 class SOL extends ModTemplate {
-	constructor(app) {
-		super(app);
+  constructor(app) {
+    super(app);
 
-		this.appname = 'SOL';
-		this.name = 'SOL';
-		this.slug = 'sol';
-		this.ticker = 'SOL';
-		this.description =
-			'Adds support for Mixin-powered Solana transfers on the Saito Network';
-		this.categories = 'Utility Cryptocurrency Finance';
+    this.appname = 'SOL';
+    this.name = 'SOL';
+    this.slug = 'sol';
+    this.ticker = 'SOL';
+    this.description = 'Adds support for Mixin-powered Solana transfers on the Saito Network';
+    this.categories = 'Utility Cryptocurrency Finance';
 
-		// MIXIN STUFF
-		this.asset_id = '64692c23-8971-4cf4-84a7-4dd1271dd887';
-		this.chain_id = '64692c23-8971-4cf4-84a7-4dd1271dd887';
-	}
+    // MIXIN STUFF
+    this.asset_id = '64692c23-8971-4cf4-84a7-4dd1271dd887';
+    this.chain_id = '64692c23-8971-4cf4-84a7-4dd1271dd887';
+  }
 
-	respondTo(type = '') {
-		if (type == 'mixin-crypto') {
-			return {
-				name: this.name,
-				ticker: this.ticker,
-				description: this.description,
-				asset_id: this.asset_id
-			};
-		}
-		return null;
-	}
+  respondTo(type = '') {
+    if (type == 'mixin-crypto') {
+      return {
+        name: this.name,
+        ticker: this.ticker,
+        description: this.description,
+        asset_id: this.asset_id
+      };
+    }
+    if (type == 'crypto-logo') {
+      if (obj?.ticker == this.ticker) {
+        return {
+          img: `/sol/img/logo.png`
+        };
+      }
+    }
+
+    return null;
+  }
 }
 
 module.exports = SOL;
