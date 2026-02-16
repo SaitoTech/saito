3c8db848a80136de79916319010d69298ea2d4c6
sanka <sankadarshana@gmail.com>
2026-02-16 08:22:01 +0000

    Refactor: remove unused imports and `handle_received_buffer` method, and update `process_incoming_buffer` usage to improve clarity and maintainability

--- rust/saito-wasm/src/saitowasm.rs
+++ rust/saito-wasm/src/saitowasm.rs
@@ -41,7 +41,6 @@ use saito_core::core::routing::io::network::{Network, PeerDisconnectType};
 use saito_core::core::routing::io::network_event::NetworkEvent;
 use saito_core::core::routing::io::storage::Storage;
 use saito_core::core::routing::peers::congestion_controller::CongestionStatsDisplay;
-use saito_core::core::routing::peers::network_peer::NetworkPeer;
 use saito_core::core::routing::peers::peer_collection::PeerCollection;
 use saito_core::core::routing_thread::{RoutingEvent, RoutingStats, RoutingThread};
 use saito_core::core::stat_thread::{StatEvent, StatThread};
