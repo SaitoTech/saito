d105a2a8cf1066541a3dd7b216caced58e834019
david.lancashire@gmail.com <trevelyan@Davids-MacBook-Pro.local>
2026-02-18 16:53:49 +0700

    fix: dynmods

--- node/scripts/dyn-mod-compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-node ./config/build/webpack.config.dynmod.cjs --entrypoint=$1
-base64 -i ./dist/dyn/web/dyn.module.js > ./dist/dyn/web/base.txt
-printf "$(cat ./dist/dyn/web/base.txt)" >> ./dist/dyn_mod.js
-
