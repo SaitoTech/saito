6308f27af3eb653424d53cbb3795e0f410339099
david.lancashire@gmail.com <trevelyan@Davids-MacBook-Pro.local>
2026-03-30 00:27:11 +0700

    fix: file removal

--- rust/saito-wasm/src/wasm_network.rs
+++ rust/saito-wasm/src/wasm_network.rs
@@ -5,9 +5,13 @@ use wasm_bindgen::JsValue;
 use crate::wasm_network_api::WasmNetworkApi;
 use crate::wasm_peer::WasmPeer;
 use crate::saitowasm::{SAITO, string_to_key};
+use crate::wasm_transaction::WasmTransaction;
 
+use log::{warn, trace, debug};
 use saito_core::core::defs::SaitoPublicKey;
-use log::warn;
+use saito_core::core::consensus_thread::ConsensusEvent;
+use saito_core::core::defs::PrintForLog;
+use saito_core::core::process::process_event::ProcessEvent;
 
 #[wasm_bindgen]
 pub struct WasmNetwork;
@@ -41,18 +45,18 @@ impl WasmNetwork {
             .read()
             .await;
 
-let connected_peers: Vec<_> = peers
-    .peers
-    .values()
-    .filter(|peer| peer.is_connected())
-    .cloned()
-    .collect();
+        let connected_peers: Vec<_> = peers
+            .peers
+            .values()
+            .filter(|peer| peer.is_connected())
+            .cloned()
+            .collect();
 
-let array = Array::new_with_length(connected_peers.len() as u32);
+        let array = Array::new_with_length(connected_peers.len() as u32);
 
-for (index, peer) in connected_peers.into_iter().enumerate() {
-    array.set(index as u32, JsValue::from(WasmPeer::new_from_peer(peer)));
-}
+        for (index, peer) in connected_peers.into_iter().enumerate() {
+            array.set(index as u32, JsValue::from(WasmPeer::new_from_peer(peer)));
+        }
 
         array
     }
@@ -86,6 +90,55 @@ for (index, peer) in connected_peers.into_iter().enumerate() {
 
         Some(WasmPeer::new_from_peer(peer))
     }
+
+
+    #[wasm_bindgen(js_name = propagateTransaction)]
+    pub async fn propagate_transaction(&self, wtx: WasmTransaction) {
+        trace!("propagate_transaction");
+
+        let mut saito = SAITO.lock().await;
+        let mut tx = wtx.clone().tx;
+
+        {
+            let wallet = saito
+                .as_ref()
+                .unwrap()
+                .routing_thread
+                .wallet_lock
+                .read()
+                .await;
+
+            tx.generate(&wallet.public_key, 0, 0);
+        }
+
+        debug!(
+            "propagating transaction: {} input: {}, output : {}",
+            tx.signature.to_hex(),
+            tx.from
+                .iter()
+                .map(|slip| format!("{}", slip))
+                .collect::<Vec<String>>()
+                .join(", "),
+            tx.to
+                .iter()
+                .map(|slip| format!("{}", slip))
+                .collect::<Vec<String>>()
+                .join(", "),
+        );
+   
+        saito
+            .as_mut()
+            .unwrap()
+            .consensus_thread
+            .process_event(ConsensusEvent::NewTransaction { transaction: tx })
+            .await;
+
+
+        //crate::saitowasm::process_new_transaction(wtx).await;
+
+
+    }
+
 }
 
 
