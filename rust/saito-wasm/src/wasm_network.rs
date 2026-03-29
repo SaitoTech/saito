7e63c6817d44b96d14fd53b98a45d9c47ad16902
david.lancashire@gmail.com <trevelyan@Davids-MacBook-Pro.local>
2026-03-29 22:54:00 +0700

    fix: network.getPeers

--- rust/saito-wasm/src/wasm_network.rs
+++ rust/saito-wasm/src/wasm_network.rs
@@ -1,5 +1,7 @@
+use js_sys::JsString;
 use wasm_bindgen::prelude::*;
 use crate::wasm_network_api::WasmNetworkApi;
+use crate::wasm_peer::WasmPeer;
 
 #[wasm_bindgen]
 pub struct WasmNetwork;
@@ -15,6 +17,16 @@ impl WasmNetwork {
     #[wasm_bindgen(getter)]
     pub fn api(&self) -> WasmNetworkApi {
         WasmNetworkApi {}
+    }
+
+    #[wasm_bindgen(js_name = getPeers)]
+    pub async fn get_peers(&self) -> js_sys::Array {
+        crate::saitowasm::get_peers().await
+    }
+
+    #[wasm_bindgen(js_name = getPeer)]
+    pub async fn get_peer(&self, key: JsString) -> Option<WasmPeer> {
+        crate::saitowasm::get_peer(key).await
     }
 }
 
