dfa1e4d6b2726c3644cf047a1064198beb7c9332
sanka <sankadarshana@gmail.com>
2026-03-31 03:18:55 +0000

    .

---
+++ node/jest.config.cjs
@@ -0,0 +1,31 @@
+const legacySpecIgnores = [
+  '<rootDir>/lib/saito/block.spec.ts',
+  '<rootDir>/lib/saito/crypto.spec.ts',
+  '<rootDir>/lib/saito/peer.spec.ts',
+  '<rootDir>/lib/saito/slip.spec.ts',
+  '<rootDir>/lib/saito/transaction.spec.ts',
+  '<rootDir>/lib/saito/core/storage-core.spec.ts',
+];
+
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/lib'],
+  testMatch: ['**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  watchPathIgnorePatterns: ['<rootDir>/dist/'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/', ...legacySpecIgnores],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/config/build/tsconfig.json',
+        diagnostics: false,
+      },
+    ],
+  },
+  moduleNameMapper: {
+    '^saito-js$': '<rootDir>/node_modules/saito-js/dist/index.node.js',
+    '^saito-js/(.*)$': '<rootDir>/node_modules/saito-js/dist/$1',
+  },
+};
