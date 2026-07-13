0db4bdb8fce946f55dcffcd5264f5200d756a391
Daniel Worlton <f8daniel@gmail.com>
2026-07-13 10:34:57 +0300

    solitrio upgrade

--- node/mods/solitrio/lib/solitrio-game-options.template.js
+++ node/mods/solitrio/lib/solitrio-game-options.template.js
@@ -7,7 +7,7 @@ module.exports = (app, mod) => {
             <fieldset class="saito-grid">
             <legend class="settings-label">Play Mode</legend>
             <input type="radio" id="auto" name="play_mode" value="auto" ${play_mode == "auto" ? "checked" : ""}/>
-            <label for="auto">Automatic <span class="note">Click a card to automatically move it to a legal slot</span></label>
+            <label for="auto">Automatic <span class="note">Click a card to automatically move it to a legal slot. Click a 2 repeatedly to cycle it through the open rows.</span></label>
             <input type="radio" id="manual" name="play_mode" value="manual" ${play_mode == "manual" ? "checked" :""}/>
             <label for="manual">Manual <span class="note">Click a card to select it, then click an empty space to move it</span></label>
             </fieldset>
