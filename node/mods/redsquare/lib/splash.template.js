909ebc264c83f0c4df33e4029b97b21da5353878
Richard Parris <github@hda0.net>
2026-07-17 21:22:34 +1000

    readded rs splash.template

---
+++ node/mods/redsquare/lib/splash.template.js
@@ -0,0 +1,9 @@
+module.exports = () => `
+  <div class="redsquare-splash-overlay" data-saito-prerendered-cta="redsquare">
+    <div class="redsquare-splash-content saito-cta">
+      <div class="saito-cta-logo redsquare-splash-logo" role="img" aria-label="Red Square"></div>
+      <div class="redsquare-splash-subtitle">PEER-TO-PEER SOCIAL</div>
+      <button class="saito-button-primary redsquare-splash-join" type="button">JOIN IN</button>
+    </div>
+  </div>
+`;
