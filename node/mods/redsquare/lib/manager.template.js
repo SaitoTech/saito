49a4d2ac654935ddd6dc84f0f2657e777d5ea777
Richard Parris <github@hda0.net>
2026-07-24 15:55:26 +1000

    fixed column widths, resiging and scolling

--- node/mods/redsquare/lib/manager.template.js
+++ node/mods/redsquare/lib/manager.template.js
@@ -3,7 +3,7 @@ module.exports = () => {
   // Header chrome is mounted separately when the active view requires it.
   // Panels are identified by data-panel for JS; visibility via hidden.
   return `
-      <div class="body">
+      <div class="body hide-scrollbar">
         <div class="list" data-panel="timeline"></div>
         <div class="list" data-panel="thread" hidden></div>
         <div class="list" data-panel="notifications" hidden></div>
