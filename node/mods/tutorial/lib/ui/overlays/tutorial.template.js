c0414c9827cc744ee2965fe78627af3b0e4c0c11
Richard <github@hda0.net>
2026-09-25 04:55:49 +0000

    Remove atr, blog, diddy, gametestsuite, status and tutorial mods

--- node/mods/tutorial/lib/ui/overlays/tutorial.template.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app, mod, tutorial_overlay = {}) => {
-
-  return `
-    <div class="tutorial-overlay-container'>
-      When the Tutorial Overlay is rendered, it will show inside the Saito Overlay
-    </div>
-  `;
-
-};
--- node/mods/tutorial06/lib/css.template.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => {
-  return `
-
-.saito-header-logo {
-        background-color: green;
-}
-
-	`;
-};
